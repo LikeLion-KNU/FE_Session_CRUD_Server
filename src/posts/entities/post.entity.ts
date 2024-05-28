2f4ae7aff4e33eda853c5702c4d8c618999e82fa
toothlessdev <toothless042@gmail.com>
2024-05-28 21:02:47 +0900

    feat: CreatedDate Column 추가

--- src/posts/entities/post.entity.ts
+++ src/posts/entities/post.entity.ts
@@ -1,11 +1,19 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+} from "typeorm";
 
 @Entity()
 export class PostModel {
     @PrimaryGeneratedColumn()
     @ApiProperty({ example: "number", description: "게시글의 ID" })
     id: number;
+
+    @CreateDateColumn()
+    createdAt: Date;
 
     @Column()
     @ApiProperty({ example: "string", description: "게시글의 제목" })
